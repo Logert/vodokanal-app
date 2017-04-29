69fb0cfc49f05672aca6f60a8a466c23b605e045
Loger <vladisl.mukhin@gmail.com>
2017-04-29 13:57:09 +0300

    отступы

--- vodokanal/urls.py
+++ vodokanal/urls.py
@@ -23,36 +23,47 @@ from main.views import ListLics, detail_lics, ListDom, DetailDom, ListNasPunkt, 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', ListLics.as_view(), name='index'),
+
     # lics
     url(r'^lics/$', ListLics.as_view(), name='list_lics'),
     url(r'^lics/(?P<lics_id>[0-9]+)/$', detail_lics, name='detail_lics'),
+
     # dom
     url(r'^dom/$', ListDom.as_view(), name='list_dom'),
     url(r'^dom/(?P<pk>[0-9]+)/$', DetailDom.as_view(), name='detail_dom'),
+
     # nas_punkt
     url(r'^nas_punkt/$', ListNasPunkt.as_view(), name='list_NasPunkt'),
     url(r'^nas_punkt/(?P<pk>[0-9]+)/$', DetailNasPunkt.as_view(), name='detail_NasPunkt'),
+
     # persons
     url(r'^persons/$', ListPersons.as_view(), name='list_persons'),
     url(r'^persons/(?P<pk>[0-9]+)/$', DetailPersons.as_view(), name='detail_persons'),
+
     # kvartiry
     url(r'^kvartiry/$', ListKvartiry.as_view(), name='list_kvartiry'),
     url(r'^kvartiry/(?P<pk>[0-9]+)/$', DetailKvartiry.as_view(), name='detail_kvartiry'),
+
     # Streets
     url(r'^streets/$', ListStreets.as_view(), name='list_streets'),
     url(r'^streets/(?P<pk>[0-9]+)/$', DetailStreets.as_view(), name='detail_streets'),
+
     # lgoty
     url(r'^lgoty/$', ListLgoty.as_view(), name='list_lgoty'),
     url(r'^lgoty/(?P<pk>[0-9]+)/$', DetailLgoty.as_view(), name='detail_lgoty'),
+
     # tip_lgoty
     url(r'^tip_lgoty/$', ListTipLgoty.as_view(), name='list_tip_lgoty'),
     url(r'^tip_lgoty/(?P<pk>[0-9]+)/$', DetailTipLgoty.as_view(), name='detail_tip_lgoty'),
+
     # pribory
     url(r'^pribory/$', ListPribory.as_view(), name='list_pribory'),
     url(r'^pribory/(?P<pk>[0-9]+)/$', DetailPribory.as_view(), name='detail_pribory'),
+
     # Marki priborov
     url(r'^marki_pribory/$', ListMarkiPribory.as_view(), name='list_marki_pribory'),
     url(r'^marki_pribory/(?P<pk>[0-9]+)/$', DetailMarkiPribory.as_view(), name='detail_marki_pribory'),
+
     # uslugi
     url(r'^uslugi/$', ListUslugi.as_view(), name='list_uslugi'),
     url(r'^uslugi/(?P<pk>[0-9]+)/$', DetailUslugi.as_view(), name='detail_uslugi'),
